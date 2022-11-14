9ecb8eb06bb471dcfbc9888ef3b502a30990247a
Dylph <dylphin020@gmail.com>
2022-11-13 19:27:20 -0600

    Fully complete MadLibs project.

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+def Madlibs():
+    food = input("What is your favorite food? (noun): ")
+    activity = input("What is your favorite activity? (verb): ")
+    color = input("What is your favorite color? (noun): ")
+    color2 = input("What is your least favorite color? (noun): ")
+    gf = input("What is your girlfriends name? (noun): ")
+    fav_part = input("What is the best part about her? (noun or verb): ")
+    school = input("What school do you go to? (noun): ")
+    subject = input("What is your favorite subject? (noun): ")
+    print('My favorite food is ' + food + '. I love going ' + activity + '. My favorite color is ' + color + ' and my least favorite color is ' + color2 + '.')
+    print('My girlfriends name is ' + gf + '. I think my favorite part about her is her ' + fav_part + '. I go to ' + school + ' and my favorite subject is ' + subject + '.')
+
+
+Madlibs()
